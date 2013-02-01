f268d7aff0cb8608326c3541b8afe81ce95f7275
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-01-31 21:10:24 -0600

    Got it all working.

--- js/jquery.dim.js
+++ js/jquery.dim.js
@@ -9,4 +9,31 @@
 
 ;(function($) {
 
+  $.fn.dim = function(opts) {
+    var $button = $(opts.button);
+
+    $('input').each(function() {
+      $(this).attr("data-valid", "false");
+    });
+
+    if ($('input').val() === "" && $('input').val().length < 1) {
+      $button.addClass('dim');
+      $button.attr('disabled', 'disabled');
+    }
+
+    $('input').on('keyup', function() {
+      if ($(this).val().length > 0) {
+        $(this).attr("data-valid", "true");
+      } else {
+        $(this).attr("data-valid", "false");
+        $button.addClass('dim');
+        $button.attr('disabled', 'disabled');
+      }
+
+      if ($('input[data-valid="false"]').length === 0) {
+        $button.removeClass('dim');
+        $button.removeAttr('disabled');
+      }
+    });
+  }
 })(window.jQuery);
